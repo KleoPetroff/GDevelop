52ea15d7ec6c3edf51efddbfa8cc1e1bc2356c4e
Florian Rival <Florian.Rival@gmail.com>
2018-09-29 18:03:06 -0400

    [WIP] Add EventsFunctionsExtensionEditor

--- newIDE/app/src/EventsSheet/index.js
+++ newIDE/app/src/EventsSheet/index.js
@@ -68,7 +68,6 @@ type Props = {|
   objectsContainer: gdObjectsContainer,
   events: gdEventsList,
   setToolbar: (?React.Node) => void,
-  updateToolbar: () => void,
   showPreviewButton: boolean,
   showNetworkPreviewButton: boolean,
   onPreview: (options: PreviewOptions) => void,
